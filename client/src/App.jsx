3ad8c1b6a41a39ca5e6278dc868034773fbd88b1
devchristines <dev.christines@gmail.com>
2025-10-20 13:43:50 +0800

    feat: add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 // import { useState } from 'react'
 import Login from './components/Login'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Test from './pages/Test'
+import Landing from './pages/Landing'
 
 function App() {
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
