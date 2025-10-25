72a81698d2b3c39203a996102a05f0c280a164ca
devchristines <dev.christines@gmail.com>
2025-10-25 21:56:03 +0800

    feat: add ENV params

--- server/app.js
+++ server/app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-// app.use(cors())
+if (config.ENV !== "live") {
+    app.use(cors())
+}
+
 app.use(express.json())
 app.use(express.static('dist'))
 
