30453471cc3e5a329235f843722d78ca6027f141
dev-nich-m2 <csapuras@gmail.com>
2026-01-28 20:43:29 +0800

    feat: update cron to every 1m

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use('/api/access', access)
 
 app.use(middleware.errorHandler)
 
-cron.schedule("* 5 * * *", () => {
+cron.schedule("* * * * *", () => {
   console.log("Running health status...");
   cronJob.checkHealth();
 });
